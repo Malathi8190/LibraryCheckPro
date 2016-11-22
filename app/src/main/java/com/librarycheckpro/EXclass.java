950b178a6894c4d29c98d1b7391eed2e1df1f3a0
Malathi8190 <malu8190@gmail.com>
2016-11-22 11:48:44 +0530

    app

--- app/src/main/java/com/librarycheckpro/EXclass.java
+++ app/src/main/java/com/librarycheckpro/EXclass.java
@@ -10,6 +10,11 @@ import android.widget.Toast;
 public class EXclass{
     public static void testToast(Context context, String Msg) {
         Toast.makeText(context, "library", Toast.LENGTH_LONG).show();    }
+    public static void testActivity(Context context) {
 
+        Intent intent = new Intent(context, Main1Activity.class);
+        context.startActivity(intent);
+
+    }
 }
 
